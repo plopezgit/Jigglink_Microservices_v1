0c12e6936cb3f2941c2539aa18ca9d997f7c02e4
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-02-21 11:50:23 +0100

    Student controller increment code

--- Student_Microservice/src/main/java/com/jigglink/student/service/controller/StudentController.java
+++ Student_Microservice/src/main/java/com/jigglink/student/service/controller/StudentController.java
@@ -5,12 +5,13 @@ import com.jigglink.student.service.model.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
@@ -22,6 +23,19 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<StudentDTO> createStudent(@Valid @RequestBody StudentDTO studentDTO, WebRequest request) {
         return ResponseEntity.ok(studentService.createStudent(studentDTO));
+    }
+
+    @Operation(summary = "It obtains all the students.")
+    @GetMapping
+    public ResponseEntity<?> getStudents() {
+        List<StudentDTO> studentsDTO = studentService.getStudents();
+        if (studentsDTO.isEmpty()) {
+            return new ResponseEntity<>(ResponseMessage.builder()
+                    .responseCode(HttpStatus.NO_CONTENT.value())
+                    .message("The students database is empty.").responseTimeStamp(new Date()).build(), HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(studentsDTO, HttpStatus.OK);
+        }
     }
 
 }
